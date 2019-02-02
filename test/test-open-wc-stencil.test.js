a5386e1168c3f67c05a7332142f04d12bd934fa3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-02-02 16:55:49 +0100

    componentOnReady implentation

--- test/test-open-wc-stencil.test.js
+++ test/test-open-wc-stencil.test.js
@@ -5,10 +5,6 @@ import {
 } from '@open-wc/testing';
 
 import {
-  nextFrame
-} from '@open-wc/testing-helpers';
-
-import {
   defineCustomElements
 } from '../dist/esm/es2017/nova.define.js';
 defineCustomElements(window);
@@ -16,23 +12,29 @@ defineCustomElements(window);
 describe('<nova-badge>', () => {
   it('Rendered by Stencil', async () => {
     const el = await fixture('<nova-badge></nova-badge>');
-    await nextFrame();
+    await el.componentOnReady();
+
     expect(el.getAttribute('class')).to.equal('hydrated');
+
     const scoreElement = el.shadowRoot.querySelector('.score');
+
     assert.notEqual(scoreElement.getAttribute('class').indexOf('one'), -1);
   });
+
   it('should have the nova badge score to 1', async () => {
     const el = await fixture('<nova-badge></nova-badge>');
-    await nextFrame();
+    await el.componentOnReady();
+
     const scoreElement = el.shadowRoot.querySelector('.score');
+
     assert.equal(scoreElement.textContent, "1");
   });
-  /*
+
   it('should have the nova badge score correct CSS class', async () => {
     const el = await fixture('<nova-badge></nova-badge>');
-    await nextFrame();
+    await el.componentOnReady();
+
     const scoreElement = el.shadowRoot.querySelector('.score');
     assert.notEqual(scoreElement.getAttribute('class').indexOf('one'), -1);
   });
-  */
 });
