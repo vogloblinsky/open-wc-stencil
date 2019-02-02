2f5f4893ad30135643c57e26a2f88445063592b1
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-02-02 20:17:28 +0100

    CI

--- karma.es5.bs.config.js
+++ karma.es5.bs.config.js
@@ -7,7 +7,20 @@ module.exports = config => {
   config.set(merge(bsSettings(config), karmaEs5Config(config), {
     browserStack: {
       project: 'open-wc-stencil',
-    }
+    },
+    browsers: [
+      'bs_win10_chrome_latest',
+      'bs_win10_firefox_latest',
+      'bs_win10_edge_latest',
+      'bs_osxmojave_safari_latest',
+      'bs_win10_ie_11', {
+        base: 'BrowserStack',
+        browser: 'Safari',
+        browser_version: '11.1',
+        os: 'OS X',
+        os_version: 'High Sierra',
+      }
+    ]
   }));
   return config;
 };
