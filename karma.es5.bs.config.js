1fa7e28277f478d2e1188424468575eab1da55ff
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-02-05 11:43:57 +0100

    More browsers

--- karma.es5.bs.config.js
+++ karma.es5.bs.config.js
@@ -15,9 +15,36 @@ module.exports = config => {
         browser_version: '11.1',
         os: 'OS X',
         os_version: 'High Sierra'
+      },
+      bs_osxhighsierra_safari_101: {
+        base: 'BrowserStack',
+        browser: 'Safari',
+        browser_version: '10.1',
+        os: 'OS X',
+        os_version: 'Sierra'
+      },
+      bs_win_chrome_52: {
+        base: 'BrowserStack',
+        browser: 'Chrome',
+        browser_version: '52.0',
+        os: 'Windows',
+        os_version: '10'
+      },
+      bs_win_firefox_62: {
+        base: 'BrowserStack',
+        browser: 'Firefox',
+        browser_version: '62.0',
+        os: 'Windows',
+        os_version: '10'
       }
     }
   }));
-  config.browsers = [...config.browsers, 'bs_osxhighsierra_safari_111'];
+  config.browsers = [
+    ...config.browsers,
+    'bs_osxhighsierra_safari_111',
+    'bs_osxhighsierra_safari_101',
+    'bs_win_chrome_52',
+    'bs_win_firefox_62'
+  ];
   return config;
 };
