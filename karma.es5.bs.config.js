ae064b2c0142b050b0a982fcde607889503cdf18
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-02-06 09:55:52 +0100

    IE10

--- karma.es5.bs.config.js
+++ karma.es5.bs.config.js
@@ -36,6 +36,13 @@ module.exports = config => {
         browser_version: '62.0',
         os: 'Windows',
         os_version: '10'
+      },
+      bs_win_ie_10: {
+        base: 'BrowserStack',
+        browser: 'IE',
+        browser_version: '10.0',
+        os: 'Windows',
+        os_version: '8'
       }
     }
   }));
@@ -44,7 +51,8 @@ module.exports = config => {
     'bs_osxhighsierra_safari_111',
     'bs_osxhighsierra_safari_101',
     'bs_win_chrome_52',
-    'bs_win_firefox_62'
+    'bs_win_firefox_62',
+    'bs_win_ie_10'
   ];
   return config;
 };
